e2959886ee18982491a212eb2d6677ea5c8be304
Lynn010 <Lynn000123@hotmail.com>
2016-07-10 23:05:59 +0800

    创建框架
    1.创建工程

---
+++ LNFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNFramework
+//
+//  Created by cong on 16/7/10.
+//  Copyright © 2016年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LNFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LNFramework
+//
+//  Created by cong on 16/7/10.
+//  Copyright © 2016年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
